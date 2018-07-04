d63b50634d63527d7dd28c1a2893adef09ecba67
YuriiShkinder <yurkaaa96@gmail.com>
2018-07-04 20:54:19 +0300

    end

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://glacial-temple-10128.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
